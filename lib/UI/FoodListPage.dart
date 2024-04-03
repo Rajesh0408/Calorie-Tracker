1c4dd587bfebd2dd7322d5682cff0b0a0893c2f1
unknown <rajesh.a0408@gmail.com>
2024-04-03 21:53:21 +0530

    initial commit

---
+++ lib/UI/FoodListPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FoodListPage extends StatefulWidget {
+  const FoodListPage({super.key});
+
+  @override
+  State<FoodListPage> createState() => _FoodListPageState();
+}
+
+class _FoodListPageState extends State<FoodListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'UI/FoodListPage.dart';
+
+void main() {
+  runApp(const FoodListPage());
+}
+
+
