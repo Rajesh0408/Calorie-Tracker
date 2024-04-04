88c4f9a83d88b4a0ad5d8a7e4b820141642b887a
unknown <rajesh.a0408@gmail.com>
2024-04-04 20:55:18 +0530

    Food List Page UI completed

--- lib/UI/FoodListPage.dart
+++ lib/UI/FoodListPage.dart
@@ -1,17 +1,142 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
+  import 'package:cloud_firestore/cloud_firestore.dart';
 
-class FoodListPage extends StatefulWidget {
-  const FoodListPage({super.key});
+  class FoodListPage extends StatefulWidget {
+    const FoodListPage({super.key});
 
-  @override
-  State<FoodListPage> createState() => _FoodListPageState();
-}
-
-class _FoodListPageState extends State<FoodListPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    @override
+    State<FoodListPage> createState() => _FoodListPageState();
   }
-}
+
+  class _FoodListPageState extends State<FoodListPage> {
+    List? list;
+    int? len;
+    int count=0;
+    // List<Integer> count;
+
+    @override
+    void initState() {
+      super.initState();
+      readData();
+    }
+
+    Future<void> readData() async {
+      CollectionReference collectionReference =
+          FirebaseFirestore.instance.collection('Foods');
+      QuerySnapshot querySnapshot = await collectionReference.get();
+
+      setState(() {
+        len = querySnapshot.docs.length;
+        list = querySnapshot.docs.map((doc) => doc.data()).toList();
+        print(list);
+      });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Calorie Tracker For Indian Food')),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 2,//len ?? 0,
+                itemBuilder: (context, index) {
+                  if (list == null || list!.isEmpty) {
+                    return const CircularProgressIndicator(); // or any other loading indicator
+                  }
+                  print(len);
+
+                  Map<String, dynamic> foodData = list![index];
+                  print(foodData['name']);
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: SizedBox(
+                            height: 50,
+                            width: 50,
+                            child: Image(
+                                image: AssetImage('assets/chappathi.jpeg'))),
+                      ),
+                      Text('${foodData['name']} (${foodData['calories']} cal)'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+
+                        children: [
+                          SizedBox(
+                            height: 32,
+                            width: 32,
+                            child: FloatingActionButton(
+                                onPressed: () {
+                                  if(count!=0) {
+                                    setState(() {
+                                      count--;
+                                    });
+
+                                  }
+                                },
+                                child: const Center(
+                                  child: Text(
+                                    '-',
+                                    style: TextStyle(fontSize: 30),
+                                  ),
+                                )),
+                          ),
+                          const SizedBox(
+                            width: 18,
+                          ),
+                          Text(
+                            '${count}',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          const SizedBox(
+                            width: 18,
+                          ),
+                          SizedBox(
+                            height: 32,
+                            width: 32,
+                            child: FloatingActionButton(
+                                onPressed: () {
+                                  setState(() {
+                                    count++;
+                                  });
+
+                                },
+                                child: const Text(
+                                  '+',
+                                  style: TextStyle(fontSize: 30),
+                                )),
+                          ),
+                          const SizedBox(
+                            width: 18,
+                          ),
+                        ],
+                      )
+
+                    ],
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+
+                      },
+                      child: const Text('Total Calories')),
+                  ElevatedButton(onPressed: () {}, child: const Text('Reset')),
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:calorie_tracker/UI/FoodListPage.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-import 'UI/FoodListPage.dart';
 
-void main() {
-  runApp(const FoodListPage());
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(apiKey: "AIzaSyA7TyZ-6C_RfEX6qZ9Z9G50wf4au-TmAkg", appId: '545120632703', messagingSenderId: "", projectId: "calorie-tracker-aea6b"),
+  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home: FoodListPage()));
 }
-
-
